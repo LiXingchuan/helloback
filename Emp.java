ad95f2921c87cd97bca35933fe4696798ba51708
lixingchuan <bj@lixingchuan.com>
2019-03-06 16:19:35 +0800

    Emp.java

--- Emp.java
+++ Emp.java
@@ -4,4 +4,14 @@ public class Emp implements Serializable{
 	private String ename;
 	System.out.println("我很伤心，不要来惹我");
 	public void class("开心");
+	
+	
+	
+	System.out.println("我是一个补丁");
+	System.out.println("我是一个补丁");
+	System.out.println("我是一个补丁");
+	System.out.println("我是一个补丁");
+	System.out.println("我是一个补丁");
+	System.out.println("我是一个补丁");System.out.println("我是一个补丁");
+	System.out.println("我是一个补丁");
 }
