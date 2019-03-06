995f98949ee290725bd7233a6ca669d2e165d2f0
lixingchuan <bj@lixingchuan.com>
2019-03-06 14:02:46 +0800

    add new mehod

--- Emp.java
+++ Emp.java
@@ -4,4 +4,11 @@ public class Emp Implements Serializable{
 	private Integer empno;
 	private String ename;
 	private sTRING job;
-}
+	
+	public static void main(String[] args){
+		System.out.println("我是李兴川");
+		System.out.println("喜欢宋海航");
+		System.out.println("我是一个小鸭子");
+	}
+
+	}
